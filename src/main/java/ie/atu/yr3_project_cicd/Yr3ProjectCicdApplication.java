80137048b147c6c0ecea9f45c63779cfda8bfd8b
G00402034 <G00402034@atu.ie>
2024-01-08 01:55:17 +0000

    added docker file

--- src/main/java/ie/atu/yr3_project_cicd/Yr3ProjectCicdApplication.java
+++ src/main/java/ie/atu/yr3_project_cicd/Yr3ProjectCicdApplication.java
@@ -2,7 +2,9 @@ package ie.atu.yr3_project_cicd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class Yr3ProjectCicdApplication {
 
